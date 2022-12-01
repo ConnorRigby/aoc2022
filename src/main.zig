3939c8d37279b8e1ed496a1fb2c2897afbd90383
Connor Rigby <connorr@hey.com>
2022-12-01 09:20:16 -0700

    day1 clean
    Signed-off-by: Connor Rigby <connorr@hey.com>

--- src/main.zig
+++ src/main.zig
@@ -33,19 +33,17 @@ const day1_part1_test_input = @embedFile("day1_part_1.txt");
 test "aoc day 1 part 1" {
     std.debug.print("\n", .{});
 
-    // elf => calorie count
-    // var acc = std.AutoHashMap(usize, usize).init(std.testing.allocator);
-
-    var acc = std.ArrayList(usize).init(std.testing.allocator);
-    defer acc.deinit();
+    var counts = std.ArrayList(usize).init(std.testing.allocator);
+    defer counts.deinit();
 
     var i:usize = 0;
     var j:usize = 0;
-    var tmp_buffer = try std.testing.allocator.alloc(u8, 255);
-    defer std.testing.allocator.free(tmp_buffer);
 
-    var tmp_acc = std.ArrayList(usize).init(std.testing.allocator);
-    defer tmp_acc.deinit(); 
+    var integer_parse_buffer = try std.testing.allocator.alloc(u8, 255);
+    defer std.testing.allocator.free(integer_parse_buffer);
+
+    var callorie_count_accumulator = std.ArrayList(usize).init(std.testing.allocator);
+    defer callorie_count_accumulator.deinit(); 
 
     var elf_id: usize = 1;
 
@@ -55,43 +53,34 @@ test "aoc day 1 part 1" {
                 var done = false;
                 var count:usize = 0;
                 while(done == false) {
-                    var value = tmp_acc.popOrNull();
+                    var value = callorie_count_accumulator.popOrNull();
                     if(value) |v| {
                         count+=v;
                     } else {done = true;}
                 }
-                // try acc.put(elf_id, count);
-                try acc.append(count);
+                try counts.append(count);
                 elf_id+=1;
-                // tmp_acc.clearAndFree();
                 continue;
             }
 
-            tmp_buffer[j] = 0;
-            // std.debug.print("{any}", .{tmp_buffer[0..j]});
-            const v = std.fmt.parseInt(usize, tmp_buffer[0..j], 10) catch {
+            integer_parse_buffer[j] = 0;
+            const v = std.fmt.parseInt(usize, integer_parse_buffer[0..j], 10) catch {
                 break;
             };
-            try tmp_acc.append(v);
-            // std.debug.print("{d}\n", .{v});
+            try callorie_count_accumulator.append(v);
             j = 0;
             continue;
         }
-        tmp_buffer[j] = day1_part1_test_input[i];
+        integer_parse_buffer[j] = day1_part1_test_input[i];
         j+=1;
     }
-    var s = acc.toOwnedSlice();
-    defer(acc.allocator.free(s));
+    var s = counts.toOwnedSlice();
+    defer(counts.allocator.free(s));
     std.sort.sort(usize, s, {}, cmpByValue);
     var highscore: usize = s[s.len-1];
     var highscore2: usize = s[s.len-2];
     var highscore3: usize = s[s.len-3];
-    // std.sort.sort(Data, x, {}, cmpByData);
 
-    // var iter = acc.iterator();
-    // while (iter.next()) |entry| {
-    //     if(entry.value_ptr.* > highscore) highscore = entry.value_ptr.*;
-    // }
     std.debug.print("highest calorie count: {d}+{d}+{d}={d}\n", .{highscore, highscore2, highscore3, highscore+highscore2+highscore3});
     // try std.testing.expectEqual(@as(usize, 24000), highscore);
     try std.testing.expectEqual(@as(usize, 69177), highscore);
