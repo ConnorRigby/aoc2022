c87597247b1e14e3646674c92fd277d6481d2e93
Connor Rigby <connorr@hey.com>
2022-12-01 10:07:05 -0700

    day1 implementation

--- day1.js
+++ day1.js
@@ -1,5 +1,25 @@
+function compareNumbers(a, b) {
+  return a - b;
+}
+
 window.run = function(input) {
-  console.log("hello, world" + input);
+  var bucket = []; // where each elf's callorie count will be stored
+  var text = input.split(/\r?\n/); // split by every line
+
+  var numCalories = 0;
+  text.forEach(element => {
+    if(element.length == 0) {
+      bucket.push(numCalories);
+      numCalories = 0;
+      return; // don't continue when finding a empty line
+    }
+    console.log(element);
+    numCalories += parseInt(element);
+  });
+  
+  bucket.sort(compareNumbers);
+  console.log(Math.max(...bucket));
+  document.getElementById("answer").innerHTML = bucket;
 }
 
 window.onload = function() {
