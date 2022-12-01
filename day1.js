eaf0f2a3772e231f6d3b14c0eb5e96e900f9cfe0
Connor Rigby <connorr@hey.com>
2022-12-01 09:45:32 -0700

    day1 boilerplate
    Signed-off-by: Connor Rigby <connorr@hey.com>

---
+++ day1.js
@@ -0,0 +1,23 @@
+window.run = function(input) {
+  console.log("hello, world" + input);
+}
+
+window.onload = function() {
+  document.getElementById("input").value=
+`1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+`
+}
